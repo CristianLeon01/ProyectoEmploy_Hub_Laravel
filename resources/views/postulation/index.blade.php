5b1431c56a2ff9c69cbd45424729945950ec9d33
Cristian <leonvalenciacristiandavid@gmail.com>
2024-02-28 16:43:42 -0500

    Actualizacion de Postulacion, Falta corregir errores de logica

--- resources/views/postulation/index.blade.php
+++ resources/views/postulation/index.blade.php
@@ -3,32 +3,35 @@
 
 <link rel="stylesheet"  href="{{ asset('/css/Postulation/postulation.css') }}">
     <div class="hoja-de-vida">
+        @forelse ($offers as $offer)
+        <h2 class="titulo-principal">{{ $offer->name_vacant }}</h2>
         <div class="campo">
-            @forelse ($offers as $offer)
-                
-            <h2>Nombre de la vacante:{{ $offer->name_vacant }}</h2>
-            <p>Universidad: Universidad Nacional</p>
-            <p>Grado: Ingeniero Informático</p>
-            <p>Año de Graduación: 2020</p>
+            <h2>Nombre de la Oferta: <span class="nombre-oferta">{{ $offer->name_vacant }}</span></h2>
+            <p><span class="span-title">Tipo de Contrato:</span> <span class="span-info">{{ $offer->contractType->contract_name }}</span></p>
+            <p><span class="span-title">Localidad:</span> <span class="span-info">{{ $offer->address }}</span></p>
+            <p><span class="span-title">Meses de experiencia:</span> <span class="span-info">{{ $offer->months_experience }}</span></p>
         </div>
-        <div class="campo">
-            <h2>Educación</h2>
-            <p>Universidad: Universidad Nacional</p>
-            <p>Grado: Ingeniero Informático</p>
-            <p>Año de Graduación: 2020</p>
-        </div>
+        @foreach ($companies as $company)
+            <div class="campo">
+                <h2>Nombre de la Empresa: <span class="nombre-oferta">{{ $company->company_name }}</span></h2>
+                <p><span class="span-title">NIT de la Empresa:</span> <span class="span-info">{{ $company->NIT }}</span></p>
+                <p><span class="span-title">Representativo Legal:</span> <span class="span-info">{{ $company->legal_representative}}</p>
+                <p><span class="span-title">Email de la empresa:</span> <span class="span-info">{{ $company->email }}</p>
+            </div>
+        @endforeach
         <div class="campo">
             <h2>Experiencia Laboral</h2>
-            <p>Empresa: XYZ Solutions</p>
-            <p>Cargo: Desarrollador Web</p>
-            <p>Periodo: 2018 - Presente</p>
+            <p><span class="span-title">Empresa:</span> <span class="span-info">XYZ Solutions</span></p>
+            <p><span class="span-title">Cargo:</span> <span class="span-info">Desarrollador Web</span></p>
+            <p><span class="span-title">Periodo:</span> <span class="span-info">2018 - Presente</span></p>
         </div>
         <div class="campo">
             <h2>Habilidades</h2>
-            <p>Programación: JavaScript, HTML, CSS</p>
-            <p>Idiomas: Inglés (Avanzado)</p>
-            <p>Soft Skills: Trabajo en equipo, Comunicación</p>
+            <p><span class="span-title">Programación:</span> <span class="span-info">JavaScript, HTML, CSS</span></p>
+            <p><span class="span-title">Idiomas:</span> <span class="span-info">Inglés (Avanzado)</span></p>
+            <p><span class="span-title">Soft Skills:</span> <span class="span-info">Trabajo en equipo, Comunicación</span></p>
         </div>
+        
         <div class="campo-CV">
             <h2>Upload Resume</h2><br>
             <label for="file-input" class="custom-file-upload">Upload CV</label><br><br>
