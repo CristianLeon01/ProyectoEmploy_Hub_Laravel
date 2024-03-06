65dda880f14d1dc2f8137af2d1302bf048e7c9b7
Diego Carranza <diego300carranza754@gmail.com>
2024-03-06 16:37:41 -0500

    Edit de users completado

--- resources/views/administrator/manage.blade.php
+++ resources/views/administrator/manage.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 @section('content')
 
-<link rel="stylesheet" href="{{ asset('/css/Administrators/Manage.css') }}">
+{{-- <link rel="stylesheet" href="{{ asset('/css/Administrators/Manage.css') }}"> --}}
 
 <br><br>
 <div class="container">
     <h2 class="TituloManage">Tabla de Usuarios Registrados</h2><br><br>
 
     <div class="table-wrapper">
-        <button class="CambiarBoton">Cambiar</button>
         <table>
             <thead>
                 <tr>
+                    <th></th>
                     <th class="Rol-th">Rol</th>
                     <th>Nombre</th>
                     <th class="Correo-th">Correo Electrónico</th>
@@ -19,18 +19,15 @@
                 </tr>
             </thead>
             <tbody>
+                @foreach ($users as $user)
                 <tr>
-                    <td>Instructor</td>
-                    <td>Juan Pérez</td>
-                    <td>juan@example.com</td>
-                    <td>2024-02-19</td>
+                    <td><a class="editRole" href="{{ route('administrator.roleEdit', ['user'=>$user]) }}">Edit</a></td>
+                    <td>{{$user->user_types->user_type_name}}</td>
+                    <td>{{$user->username}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->created_at}}</td>
                 </tr>
-                <tr>
-                    <td>Reclutador</td>
-                    <td>María García</td>
-                    <td>maria@example.com</td>
-                    <td>2024-02-18</td>
-                </tr>
+                @endforeach
                 <!-- Aquí puedes agregar más filas según sea necesario -->
             </tbody>
         </table>
