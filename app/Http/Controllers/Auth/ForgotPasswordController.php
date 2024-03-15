a208e7d56b95255e420b74eb171b861ead56b004
cristian <leonvalenciacristiandavid@gmail.com>
2024-03-15 16:11:03 -0500

    Primer MERGE

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,8 +4,39 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
-    
+    // use SendsPasswordResetEmails;
+
+    // /**
+    //  * Show the form for requesting a password reset link.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function showLinkRequestForm()
+    // {
+    //     return view('auth.email');
+    // }
+
+    // /**
+    //  * Send a reset link to the given user.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @return \Illuminate\Http\RedirectResponse
+    //  */
+    // public function sendResetLinkEmail(Request $request)
+    // {
+    //     $this->validateEmail($request);
+
+    //     $response = $this->broker()->sendResetLink(
+    //         $request->only('email')
+    //     );
+
+    //     return $response == Password::RESET_LINK_SENT
+    //                 ? back()->with('status', trans($response))
+    //                 : back()->withErrors(['email' => trans($response)]);
+    // }
 }
